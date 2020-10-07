8e3fd40489e6ec1c15461670fd6b4ea521afc608
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2020-10-06 20:21:51 -0500

    Mi primer commit

---
+++ src/test/java/com/bcp/PidT6Cn02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidT6Cn02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
